2356d81ec86a50f7fdd982e42d30671f95b003ec
Orden14 <92043415+Orden14@users.noreply.github.com>
2026-05-04 11:31:41 +0200

    mfe-product (#3)
    Co-authored-by: ahucha <ahuchard91@gmail.com>

---
+++ mfe-product/webpack.config.js
@@ -0,0 +1,49 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+  output: {
+    publicPath: 'auto',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      shared: path.resolve(__dirname, '../shared'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-react'] },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfe_product',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ProductApp': './src/ProductApp',
+      },
+      shared: {
+        react:       { singleton: true, requiredVersion: '^18.2.0' },
+        'react-dom': { singleton: true, requiredVersion: '^18.2.0' },
+      },
+    }),
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+  ],
+};
